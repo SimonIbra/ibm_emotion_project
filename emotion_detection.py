fa0bebe5eeb9434cbbef4e1b6d28e6a9d463174a
SimonIbra <simon.r.ibrahim@gmail.com>
2025-03-20 14:18:20 -0400

    Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,16 +1,12 @@
 import requests  # Import the requests library to handle HTTP requests
-<<<<<<< HEAD
 import json
 
-=======
-    
->>>>>>> 417b25c3faaa5cf133b9a98d336f36a573f6afb8
 def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)  # Send a POST request to the API with the text and headers
-<<<<<<< HEAD
+
     formatted_response = json.loads(response.text) # Parsing the JSON response from the API
 
     # Extracting the emotions from the response
@@ -43,6 +39,5 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
         'sadness': sadness_score,
         'dominant_emotion': dominant_emotion
     }
-=======
+
     return response.text  # Return the response text from the API
->>>>>>> 417b25c3faaa5cf133b9a98d336f36a573f6afb8
